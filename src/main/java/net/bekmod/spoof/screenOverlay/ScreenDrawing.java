c0319bb9e2a78497b573cb1ace477e10c6f7dfc1
Ulugbek Shukurullaev <ulug0905by@gmail.com>
2023-04-23 01:37:01 +0500

    first Commit

--- src/main/java/net/bekmod/spoof/screenOverlay/ScreenDrawing.java
+++ src/main/java/net/bekmod/spoof/screenOverlay/ScreenDrawing.java
@@ -5,7 +5,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
